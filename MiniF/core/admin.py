d401fc2183e47a126eaff8427071f782ef633f54
DianaHosh <diana.hoshovska.shi.2023@lpnu.ua>
2025-09-06 22:10:30 +0300

    [Authorization] Added UserProfile model
    Defined a UserProfile model according to the database schema and task requirements. Created a migration file for the database.

--- MiniF/core/admin.py
+++ MiniF/core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import UserProfile
 
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(UserAdmin):
+    model = UserProfile
+
+    list_display = ("username", "email", "first_name", "last_name", "user_phone", "is_staff", "updated_at")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('user_phone', 'updated_at')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("user_phone",)}),
+    )
+
+    readonly_fields = ("updated_at",)
--- MiniF/users/models.py
+++ MiniF/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class UserProfile(AbstractUser):
+    user_phone = models.CharField(max_length=15, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField(unique=True)
