0bb43408c5d1eb204df254d5e020a0da75db1a35
DianaHosh <diana.hoshovska.shi.2023@lpnu.ua>
2025-09-09 13:40:43 +0300

    Updated models.py
    Added USERNAME_FIELD and REQUIRED_FIELDS.

--- MiniF/users/models.py
+++ MiniF/users/models.py
@@ -6,3 +6,9 @@ class UserProfile(AbstractUser):
     user_phone = models.CharField(max_length=15, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
     email = models.EmailField(unique=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
