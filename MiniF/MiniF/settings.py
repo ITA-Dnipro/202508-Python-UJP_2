d63bdf0beb2c645ea6768a9dfec3c32060fa3a9b
RomanSeniv <roman.seniv.shi.2023@lpnu.ua>
2025-09-11 14:45:03 +0300

    Create sign up

--- MiniF/MiniF/settings.py
+++ MiniF/MiniF/settings.py
@@ -22,10 +22,23 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
 
     "rest_framework",
+    "rest_framework.authtoken",
+
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
+
     "core",
     "users",
+    "communications",
+    "dashboard",
+    "profiles",
+    "projects",
 ]
 
 MIDDLEWARE = [
@@ -36,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "MiniF.urls"
@@ -85,3 +99,17 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = "users.UserProfile"
+SITE_ID = 1
+
+REST_AUTH = {
+    "REGISTER_SERIALIZER": "users.serializers.UserRegistrationSerializer",
+}
+
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_USERNAME_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_CONFIRM_EMAIL_ON_GET = False
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+DEFAULT_FROM_EMAIL = "no-reply@example.com"
--- MiniF/MiniF/urls.py
+++ MiniF/MiniF/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from core.views import health
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/health/", health),
+    path("api/", include("users.urls"))
 ]
