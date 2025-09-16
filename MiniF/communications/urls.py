fee2a22c3349edb334dc0b27a079009aa6adef35
ilia <fleshgolub@gmail.com>
2025-09-16 14:21:49 +0300

    somme code refactor

--- MiniF/communications/urls.py
+++ MiniF/communications/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("api/", include("communications.api_urls")),
-    path("api/", include("communications.api_urls")),
     path("", views.index, name="index"),
     path("<str:room_name>/", views.room, name="room"),
 ]
