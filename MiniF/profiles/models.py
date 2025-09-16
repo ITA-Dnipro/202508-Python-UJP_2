7be0f91f14449df50544b6d665d0a0ebba30cfc3
DianaHosh <146379540+DianaHosh@users.noreply.github.com>
2025-09-16 16:11:46 +0300

    [Profile] Implementing Viewing Functionality for Startup Profiles (#95)
    * [Profile] Implementing Viewing Functionality for Startup Profiles
    
    Created Industry model and updated StartupProfile with the needed fields. Added Industry model to the admin panel. Also added two URLs regarding viewing startup profiles. Added filtering and searching functionality to the startup profiles. Wrote a couple of tests to ensure correct functionality.
    
    * Moved templates to profiles folder

--- MiniF/profiles/models.py
+++ MiniF/profiles/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.utils import timezone
 
+
+class Industry(models.Model):
+    industry_name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.industry_name
+
+
 class StartupProfile(models.Model):
     """
     Startup profile model.
@@ -12,7 +20,7 @@ class StartupProfile(models.Model):
     company_name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     website = models.URLField(blank=True, null=True)
-    industry_id =models.IntegerField()
+    industry_id = models.ForeignKey(Industry, on_delete=models.CASCADE)
     LOCATION_CHOICES = [
         ("Cherkasy", "Cherkasy"),
         ("Chernihiv", "Chernihiv"),
@@ -41,4 +49,13 @@ class StartupProfile(models.Model):
         ("Zhytomyr", "Zhytomyr"),]
     location = models.CharField(max_length=50, choices=LOCATION_CHOICES)
     created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_at = models.DateTimeField(null=True, blank=True, auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["industry_id"]),
+            models.Index(fields=["location"]),
+        ]
+
+    def __str__(self):
+        return self.company_name
