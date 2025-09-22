c3f9aef77cd1c67ba4981c603d2501aa2bd8a22c
Norland <55491714+Gazam2006@users.noreply.github.com>
2025-09-22 10:00:29 +0300

    Update permissions.py

--- MiniF/users/permissions.py
+++ MiniF/users/permissions.py
@@ -5,7 +5,6 @@ class IsStartupRole(BasePermission):
     def has_permission(self, request, view):
         token = getattr(request, "auth", None)
         role = getattr(token, "payload", {}).get("role") if token else None
-        # Pylance підсвічує? fallback:
         if role is None and hasattr(request.user, "is_authenticated") and request.user.is_authenticated:
             role = getattr(request.user, "role", None)
         return role == "startup"
