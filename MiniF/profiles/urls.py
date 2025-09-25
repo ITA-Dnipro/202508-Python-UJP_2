6faedb0a1635b18a7611e2c14e74d32061b454a2
Mark1707g <marklubarskij@gmail.com>
2025-09-25 17:18:09 +0300

    Create API endpoints to list all startups an investor has saved and to manage these saved entries.

--- MiniF/profiles/urls.py
+++ MiniF/profiles/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StartupProfileViewSet, StartupProfileCreateView, InvestorProfileCreateView
+from .views import StartupProfileViewSet, StartupProfileCreateView, InvestorProfileCreateView, SaveProjectView, SavedProjectListView, UnsaveProjectView
 
 router = DefaultRouter()
 router.register(r"startup-profiles", StartupProfileViewSet, basename="startupprofile")
@@ -9,4 +9,7 @@ urlpatterns = [
     path("", include(router.urls)),
     path("startup/", StartupProfileCreateView.as_view(), name="startup-create"),
     path("investor/", InvestorProfileCreateView.as_view(), name="investor-create"),
+    path("investor/saved-projects/", SavedProjectListView.as_view(), name="saved-projects"),
+    path("investor/saved-projects/unsave/", UnsaveProjectView.as_view(), name="unsave-project"),
+    path("investor/save-project/", SaveProjectView.as_view(), name="save-project"),
 ]
