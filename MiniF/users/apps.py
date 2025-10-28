2aa675a43dbbb6b68c0688438fbf353e815da8bf
Andrut-alt <voloshyn.andriy2006@gmail.com>
2025-10-28 13:40:55 +0200

    added new changes

--- MiniF/users/apps.py
+++ MiniF/users/apps.py
@@ -1,11 +1,20 @@
-#pylint:disable=import-outside-toplevel
 from django.apps import AppConfig
+import logging
 
-class UsersConfig(AppConfig): 
-    
+logger = logging.getLogger(__name__)
+
+class UsersConfig(AppConfig):
+    """Configuration class for the users app.
+    Loads and registers signals on startup.
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
     def ready(self):
-       
-        import users.signals
+        """Import user-related signal handlers when the app is ready."""
+        try:
+            import users.signals  # pylint: disable=import-outside-toplevel
+            logger.debug("Users app ready: signals successfully imported.")
+        except ImportError as e:
+            logger.warning("Failed to import users.signals: %s", e)
