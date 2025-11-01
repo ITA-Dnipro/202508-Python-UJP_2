8c4c9105660a5218288e13e14123a0fde7415b8b
Ostap Mruts <gazam502dam@gmail.com>
2025-11-02 01:18:18 +0200

    Add limited status choices ('open', 'close') to StartupProject model

--- MiniF/projects/models.py
+++ MiniF/projects/models.py
@@ -5,16 +5,25 @@ from django.utils import timezone
 class StartupProject(models.Model):
     """
     Startup project model.
-
-    Notes:
-    - Added default=0 to the likes field.
     """
 
-    startup_profile_id = models.ForeignKey("profiles.StartupProfile", on_delete=models.CASCADE)
+    class Status(models.TextChoices):
+        OPEN = "open", "Open"
+        CLOSED = "closed", "Closed"
+
+    startup_profile_id = models.ForeignKey(
+        "profiles.StartupProfile",
+        on_delete=models.CASCADE,
+        related_name="projects"
+    )
     title = models.CharField(max_length=255)
     likes = models.IntegerField(default=0)
     description = models.TextField()
-    status = models.TextField()
+    status = models.CharField(
+        max_length=10,
+        choices=Status.choices,
+        default=Status.OPEN
+    )
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(null=True, auto_now=True)
 
